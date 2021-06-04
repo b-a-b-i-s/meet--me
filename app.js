672759bfd760b7cfa221332503eedc0ea0549e03
Babis Skeparnakos <63171080+b-a-b-i-s@users.noreply.github.com>
2021-06-05 02:46:01 +0300

    favicon

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const logger = require('morgan');
 const createError = require('http-errors');
-const favicon = require('serve-favicon')
+// const favicon = require('serve-favicon')
 
 const app = express()
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use(logger('dev'));
 app.engine('hbs', exphbs({extname: '.hbs'}));
