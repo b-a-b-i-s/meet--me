703a4728e7a5c1a5c803717b2f3064d8a951af99
Babis Skeparnakos <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-26 04:23:17 +0200

    fix security helmet bugs

---
+++ public/js/loggedin.js
@@ -0,0 +1,17 @@
+document.querySelector('#home-ref').addEventListener('mousedown', logMouseButton);
+    document.querySelector('#create-ref').addEventListener('mousedown', logMouseButton);
+    document.querySelector('#mymeetings-ref').addEventListener('mousedown', logMouseButton);
+    document.querySelector('#logout').addEventListener('mousedown', logMouseButton);
+
+    function logMouseButton(e) {   
+        if (typeof e === 'object') {
+            switch (e.button) {
+            case 0:
+                location.href=e.target.dataset.path;
+                break;
+            case 1:
+                window.open(e.target.dataset.path,'_blank');
+                break;
+            }
+        }
+    }
