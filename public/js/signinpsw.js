79709f0705d2c01d968f3b2645335a8b1dedacaa
Babis <63171080+b-a-b-i-s@users.noreply.github.com>
2022-01-26 04:09:24 +0200

    fix security helmet bugs

--- public/js/signinpsw.js
+++ public/js/signinpsw.js
@@ -10,3 +10,19 @@ togglePasswordSignIn.addEventListener('click', function (e) {
 });
 
 
+document.querySelector('#home-ref').addEventListener('mousedown', logMouseButton);
+    document.querySelector('#create-ref').addEventListener('mousedown', logMouseButton);
+
+    function logMouseButton(e) {   
+        if (typeof e === 'object') {
+            switch (e.button) {
+            case 0:
+                location.href=e.target.dataset.path;
+                break;
+            case 1:
+                window.open(e.target.dataset.path,'_blank');
+                break;
+            }
+        }
+    }
+
